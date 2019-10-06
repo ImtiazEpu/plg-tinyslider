ae05af5575ff6388351c0d9c64d408d622f9c930
Imtiaz Epu <imtiazepu@programmer.net>
2019-10-06 12:48:15 +0600

    initial commit

---
+++ Tinyslider.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: TinySlider
+Plugin URL:
+Description:
+Version: 1.0
+Author: Imtiaz Epu
+Author URI: https://www.imtiazepu.com
+License: GPLv2 or later
+Text Domain:tinyslider
+Domain Path: /languages/
+*/
+
+
+class Tinyslider {
+	/**
+	 * Tinyslider constructor.
+	 */
+	public function __construct() {
+		add_action( "plugin_loaded", array( $this, "tinys_load_textdomain" ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'tinys_assets' ) );
+		add_shortcode( 'tslider', array( $this, 'tinys_shortcode_tslider' ) );
+		add_shortcode( 'tslider', array( $this, 'tinys_shortcode_tslide' ) );
+	}
+
+	/**
+	 * Load text domain or translator
+	 */
+	public function tinys_load_textdomain() {
+		load_plugin_textdomain( 'tinyslider', false, dirname( __FILE__ ) . "/languages" );
+	}//end method tinys_load_textdomain
+
+	public function tinys_assets() {
+		wp_enqueue_style( 'tinyslider-css', '//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/tiny-slider.css', null, '1.0' );
+		wp_enqueue_script( 'tinyslider-js', '//cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js', null, '1.0', true );
+		wp_enqueue_script( 'tinyslider-main-js', plugin_dir_url( __FILE__ ) . "/assets/js/main.js", array( 'jquery' ), '1.0', true );
+	}
+
+	/**
+	 * @param $arguments
+	 * @param $content
+	 *
+	 */
+	public function tinys_shortcode_tslider( $arguments, $content ) {
+
+	}//end method tinys_shortcode_tslider
+
+	/**
+	 * @param $arguments
+	 */
+	public function tinys_shortcode_tslide( $arguments ) {
+
+	}//end method tinys_shortcode_tslide
+}
+
+new Tinyslider();
